59aef1a33cb99d26aa98303c148a531e3d8bc9ab
Ed Newman <newmanem23@gmail.com>
2023-07-11 18:57:58 -0400

    Add div grid to canvas

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 const canvas = document.querySelector('#canvas');
+
+for (let i = 0; i < 16; i++) {
+    let row = document.createElement("div");
+    for (let j = 0; j < 16; j++) {
+        let cell = document.createElement("div");
+        cell.classList.add("canvas");
+        row.appendChild(cell);
+    }
+    row.classList.add("canvas");
+    canvas.appendChild(row);
+}

49bf78ef90bc47cb164ae08f51f42874505c62a8
Ed Newman <newmanem23@gmail.com>
2023-07-11 22:09:26 -0400

    Add rainbow button functionality

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ const canvas = document.querySelector('#canvas-container');
 const slider = document.querySelector('.slider');
 slider.addEventListener('change', resetGrid);
 
-const clear = document.querySelector('#clear');
-clear.addEventListener('click', resetGrid);
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', resetGrid);
 
+let rainbowTurnedOn = false;
+const rainbowButton = document.querySelector('#rainbow');
+rainbowButton.addEventListener('click', () => {
+    rainbowTurnedOn = !rainbowTurnedOn;
+    if (rainbowButton.classList.contains("gradient")) {
+        rainbowButton.classList.remove("gradient");
+    } else {
+        rainbowButton.classList.add("gradient");
+    }
 
+});
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 resetGrid();
 
@@ -39,5 +51,10 @@ function resetGrid() {
 
 
 function shade(e) {
-    if (e.buttons) e.target.style.backgroundColor = "black";
+    let newColor = "black"
+    if (rainbowTurnedOn) {
+        let index = Math.floor(Math.random() * colors.length);
+        newColor = colors[index];
+    }
+    if (e.buttons) e.target.style.backgroundColor = newColor;
 }

b930b618a79439853c22d742c6f6acb4f1b12893
Ed Newman <newmanem23@gmail.com>
2023-07-11 21:40:59 -0400

    Reformat js function order and format controls css

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+
 const canvas = document.querySelector('#canvas-container');
+const slider = document.querySelector('.slider');
+slider.addEventListener('change', resetGrid);
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', resetGrid);
+
+
+
+resetGrid();
 
 function resetGrid() {
+    // Need to delete previous cells upon resetting the grid
     while (canvas.firstChild) {
         canvas.removeChild(canvas.firstChild);
     }
+
+    // Populate the grid with div cells
     let gridSize = document.querySelector('.slider').valueAsNumber;
     for (let i = 0; i < gridSize; i++) {
         let row = document.createElement("div");
@@ -17,16 +30,14 @@ function resetGrid() {
     }
     let label = document.querySelector(".grid-size");
     label.innerText = `${gridSize} x ${gridSize}`;
+
+    // Have to redo the event listener for shading cells here. We have
+    // deleted the elements that previously had the listener, so we need a new one
     let cells = document.querySelectorAll('.canvas');
     cells.forEach(cell => cell.addEventListener('mouseover', shade));
 }
 
+
 function shade(e) {
     if (e.buttons) e.target.style.backgroundColor = "black";
 }
-
-const slider = document.querySelector('.slider');
-slider.addEventListener('change', resetGrid);
-resetGrid();
-const clear = document.querySelector('#clear');
-clear.addEventListener('click', resetGrid);
